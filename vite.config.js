fad12769acc279410389bffebeaa86cc7be5eb01
daya444 <dayavelusamy@gmail.com>
2025-05-05 16:07:08 +0530

    Fix base path and rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- Add this line
+  base: '', // 👈 this tells Vite to use relative paths (critical!)
 })
